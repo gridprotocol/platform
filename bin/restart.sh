402df1c6242bd711db76a9434d4b0395140a0abd
rocky <rocky@memoio.io>
2024-10-12 16:00:32 +0800

    fix topup and restart, use  in shell

--- bin/restart.sh
+++ bin/restart.sh
@@ -2,5 +2,5 @@
 
 ps -ef | grep platform | grep -v 'color' | awk '{print $2}' | xargs kill -9
 
-nohup ./platform daemon run -c dev > log 2>&1 &
+nohup ./platform daemon run -c $1 > log 2>&1 &
 
--- bin/restart_sepo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ps -ef | grep platform | grep -v 'color' | awk '{print $2}' | xargs kill -9
-
-nohup ./platform daemon run -c sepo > log 2>&1 &
-
